a771f0b5eed923355c75946e2911a9467a942363
yyyyyyyyuhan <147095674+yyyyyyyyuhan@users.noreply.github.com>
2023-10-15 23:32:39 -0500

    Image.R

--- code/Image.R
+++ code/Image.R
@@ -19,3 +19,31 @@ abline(h = 0, col = "red")
 residuals_ridge <- y - y_hat_ridge
 plot(y_hat_ridge, residuals_ridge, main = "Residual Plot", xlab = "ridge_predict", ylab = "Residuals")
 abline(h = 0, col = "red")
+
+# Compare graph 
+categories <- c("MSE", "AIC", "BIC")
+MSE <- c(log(mse_lasso), log(mse_ridge), log(MSE_ml))
+AIC <- c(log(pseudo_AIC_lasso), log(pseudo_AIC_ridge), log(AIC(ml)))
+BIC <- c(log(bic_lasso), log(bic_ridge), log(BIC(ml)))
+model_kind <- c("lasso", "ridge", "ml")
+
+df <- data.frame(categories, MSE, AIC, BIC)
+
+
+bar_heights <- as.matrix(df[, 2:4])
+bar_names <- df$categories
+bar_colors <- c("red", "blue", "green")  # Customize bar colors if needed
+
+# Create the barplot
+barplot(
+  bar_heights,
+  beside = TRUE,        # Stack bars beside each other
+  names.arg = bar_names,
+  col = bar_colors,
+  xlab = "Categories",
+  ylab = "Values",
+  main = "MSE, AIC, and BIC of Three Model(log(MSE/AIC/BIC))"
+)
+
+# Add a legend
+legend("topleft", legend = model_kind, fill = bar_colors)
